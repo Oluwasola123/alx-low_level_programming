4d35264a7a4b4ac8a88ea22f15add0dc8c24a5fd
Oluwasola123 <you@oluwasolaoyeleye@gmail.com>
2023-07-23 15:26:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints base 16 numbers
+ * Return: 0.
+ */
+int main(void)
+{
+char abd;
+for (abc = '0'; abc <= '9'; abc++)
+putchar(abc);
+for (abc = 'a'; abc <= 'f'; abc++)
+putchar(abc);
+putchar('\n');
+return (0);
+}
