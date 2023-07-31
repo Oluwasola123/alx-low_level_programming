a9495942490edc2c5dadceef8d29876895b67891
oluwasola123 <you@oluwasolaoyeleye@gmail.com>
2023-07-31 15:53:41 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+int u;
+
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+	return (u);
+}
